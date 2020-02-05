9169774fb27a5a98ce15e4dac48fe1ae943b0d33
sh1r00 <rasisg@gmail.com>
2020-02-04 17:38:18 -0800

    .

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://localhost'),
 
 
     /* provide access to the usage of CDN settings for production
